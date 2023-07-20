216a08184f18fae522ffc299d831a43b418e2f5e
iakshayhere <akshaymayekar@Akshays-MacBook-Pro.local>
2023-07-20 16:57:56 +0530

    added scatter bubbles

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,16 @@ import { render } from "react-dom";
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 import highcharts3d from "highcharts/highcharts-3d";
+import highchartsMore from "highcharts/highcharts-more"; // Import highcharts-more
 highcharts3d(Highcharts);
+highchartsMore(Highcharts); 
 
 function App() {
   const [dependentStateVar, setDependentStateVar] = useState(0);
   const [options, setOptions] = useState({
     chart: {
       renderTo: "container",
-      //margin: 100,
-      type: "scatter3d",
+      type: "bubble", // Change the chart type to bubble
       animation: false,
       options3d: {
         enabled: true,
@@ -28,17 +29,24 @@ function App() {
       }
     },
     title: {
-      text: "Draggable box"
+      text: "Bubble Chart with Radial Gradient"
     },
     subtitle: {
       text: "Click and drag the plot area to rotate in space"
     },
     plotOptions: {
-      scatter: {
-        width: 10,
-        height: 10,
-        depth: 10
-      }
+      bubble: {
+        depth: 10, // Add depth to the bubbles
+        marker: {
+          fillColor: {
+            radialGradient: { cx: 0.4, cy: 0.3, r: 0.7 },
+            stops: [
+              [0, "rgba(255, 0, 0, 0.5)"], // First color stop at 50% opacity (red)
+              [1, "rgba(255, 0, 0, 1)"], // Second color stop at 100% opacity (red)
+            ]
+          },
+        },
+      },
     },
     yAxis: {
       min: 0,
@@ -65,125 +73,25 @@ function App() {
         accessibility: {
           exposeAsGroupOnly: true
         },
-        marker: {
-          symbol: dependentStateVar % 2 === 0 ? "square" : "circle",
-          fillColor: dependentStateVar % 2 === 0 ? "blue" : "red",
-        },
         data: [
-          [1, 6, 1],
-          [8, 7, 1],
-          [1, 3, 1],
-          [4, 6, 1],
-          [5, 7, 1],
-          [6, 9, 1],
-          [7, 0, 1],
-          [2, 3, 1],
-          [3, 9, 1],
-          [3, 6, 1],
-          [4, 9, 1],
-          [2, 3, 1],
-          [6, 9, 1],
-          [0, 7, 1],
-          [7, 7, 1],
-          [7, 2, 1],
-          [0, 6, 2],
-          [4, 6, 1],
-          [3, 7, 1],
-          [0, 1, 1],
-          [2, 8, 1],
-          [2, 3, 1],
-          [6, 4, 1],
-          [3, 5, 1],
-          [7, 9, 1],
-          [3, 1, 7],
-          [4, 4, 1],
-          [3, 6, 2],
-          [3, 1, 6],
-          [6, 8, 5],
-          [6, 6, 7],
-          [4, 1, 1],
-          [7, 2, 7],
-          [7, 7, 0],
-          [8, 8, 9],
-          [9, 4, 1],
-          [8, 3, 4],
-          [9, 8, 9],
-          [3, 5, 3],
-          [0, 2, 4],
-          [6, 0, 2],
-          [2, 1, 3],
-          [5, 8, 9],
-          [2, 1, 1],
-          [9, 7, 6],
-          [3, 0, 2],
-          [9, 9, 0],
-          [3, 4, 8],
-          [2, 6, 1],
-          [8, 9, 2],
-          [7, 6, 5],
-          [6, 3, 1],
-          [9, 3, 1],
-          [8, 9, 3],
-          [9, 1, 0],
-          [3, 8, 7],
-          [8, 0, 0],
-          [4, 9, 7],
-          [8, 6, 2],
-          [4, 3, 0],
-          [2, 3, 5],
-          [9, 1, 4],
-          [1, 1, 4],
-          [6, 0, 2],
-          [6, 1, 6],
-          [3, 8, 8],
-          [8, 8, 7],
-          [5, 5, 0],
-          [3, 9, 6],
-          [5, 4, 3],
-          [6, 8, 3],
-          [0, 1, 5],
-          [6, 7, 3],
-          [8, 3, 2],
-          [3, 8, 3],
-          [2, 1, 6],
-          [4, 6, 7],
-          [8, 9, 9],
-          [5, 4, 2],
-          [6, 1, 3],
-          [6, 9, 5],
-          [4, 8, 2],
-          [9, 7, 4],
-          [5, 4, 2],
-          [9, 6, 1],
-          [2, 7, 3],
-          [4, 5, 4],
-          [6, 8, 1],
-          [3, 4, 0],
-          [2, 2, 1],
-          [5, 1, 1],
-          [9, 9, 1],
-          [6, 9, 1],
-          [8, 4, 1],
-          [4, 1, 1],
-          [6, 2, 1],
-          [0, 4, 1],
-          [3, 5, 1],
-          [6, 9, 1],
-          [1, 9, 1]
+          [1, 6, 1], // Data point 1: x=1, y=6, z=1 (size of bubble)
+          [8, 7, 1], // Data point 2: x=8, y=7, z=1 (size of bubble)
+          [1, 3, 1], // Data point 3: x=1, y=3, z=1 (size of bubble)
+          // ... (more data points)
         ]
       }
     ]
   });
 
   return (
-    <div style={{width:1000,height:2000}}>
+    <div style={{ width: 1000, height: 800 }}>
       <button
         onClick={() => {
           setDependentStateVar((prev) => ++prev);
           setOptions({
             zAxis: {
               title: {
-                text: dependentStateVar
+                text: `New Title ${dependentStateVar}`
               }
             }
           });
@@ -193,7 +101,7 @@ function App() {
       </button>
       <HighchartsReact highcharts={Highcharts} options={options} />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
